8c6eaa8229571c487e08e10e533f78b8dd09abc8
Jackson Jones <63122233+JacksonKJones@users.noreply.github.com>
2023-12-11 10:38:22 -0500

    Time: 2250 ms (25.40%), Space: 14.2 MB (28.88%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,12 +1,14 @@
 class Solution(object):
+    
     def twoSum(self, nums, target):
-        """
-        :type nums: List[int]
-        :type target: int
-        :rtype: List[int]
-        """
+        
+        '''
+        I will check for the target by indexing the first integer in the array
+        and then checking if it adds together with any of the other values to match
+        the target. If it does not match, the code will repeat, starting with the next integer.
+        '''
+        
         for i in range(len(nums) - 1):
             for j in range(i + 1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
-                
